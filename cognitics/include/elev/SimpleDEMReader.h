fa8defaf5df4cf251113670fe90da725b08ef472
Kevin Bentley <kbentley@cognitics.net>
2019-05-20 19:50:04 -0600

    Simple reader for DEMs

---
+++ cognitics/include/elev/SimpleDEMReader.h
@@ -0,0 +1,94 @@
+/*************************************************************************
+Copyright (c) 2019 Cognitics, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+****************************************************************************/
+
+#pragma once
+
+#pragma warning ( push )
+#pragma warning ( disable : 4251 )        // C4251: 'GDALColorTable::aoEntries' : class 'std::vector<_Ty>' needs to have dll-interface to be used by clients of class 'GDALColorTable'
+#include <gdal.h>
+#include <gdal_priv.h>
+#include <ogr_api.h>
+#include <ogr_spatialref.h>
+#pragma warning ( pop )
+#include "ccl/ObjLog.h"
+
+namespace elev
+{
+    class SimpleDEMReader
+    {
+        GDALDataset *gdal_dataset;
+        std::string filename;                    //!< GIS data filename
+        ccl::ObjLog log;
+        int height;
+        int width;
+        int depth;
+        int windowTop;
+        int windowBottom;
+        int windowLeft;
+        int windowRight;
+        bool windowedMode;
+        double spacing_x;                    //!< coordinate distance between post x-values
+        double spacing_y;                    //!< coordinate distance between post y-values
+        double rotation_x;                    //!< rotation between post x-values
+        double rotation_y;                    //!< rotation between post y-values
+        double bound_x_low;                    //!< x-axis low boundary
+        double bound_x_high;                //!< x-axis high boundary
+        double bound_y_low;                    //!< y-axis low boundary
+        double bound_y_high;                //!< y-axis high boundary
+        double geo_bound_x_low;                //!< x-axis low boundary (geographic)
+        double geo_bound_x_high;            //!< x-axis high boundary (geographic)
+        double geo_bound_y_low;                //!< y-axis low boundary (geographic)
+        double geo_bound_y_high;            //!< y-axis high boundary (geographic)
+
+        std::string reftype;                    //!< application reference (projection) type (default is "WGS84")
+        OGRSpatialReference *file_srs;            //!< datafile spatial reference
+        OGRSpatialReference *app_srs;            //!< application spatial reference
+        OGRCoordinateTransformation *file_ct;    //!< app to file transformation
+        OGRCoordinateTransformation *app_ct;    //!< file to app transformation
+        void BuildCoordinateTransformations();
+
+    public:
+        ~SimpleDEMReader();
+        SimpleDEMReader(const std::string &filename, 
+            OGRSpatialReference destinationSRS);
+        SimpleDEMReader(const std::string &filename,
+            OGRSpatialReference destinationSRS,
+            int windowTop,
+            int windowBottom,
+            int windowRight,
+            int windowLeft);
+
+        int getWidth() const;
+        int getHeight() const;
+        double getPostSpacingX() { return spacing_x; }
+        double getPostSpacingY() { return spacing_y; }
+
+        void getMBR(double &north, double &south, double &east, double &west);
+        // Convert each post to a double value and return it as an array
+        bool getGrid(std::vector<double> &grid);
+
+        bool Open();
+    };
+
+
+
+}
---
+++ cognitics/src/elev/SimpleDEMReader.cpp
@@ -0,0 +1,229 @@
+
+#include "elev/SimpleDEMReader.h"
+
+
+namespace elev {
+    SimpleDEMReader::~SimpleDEMReader()
+    {
+        if(gdal_dataset)
+            GDALClose((GDALDatasetH)gdal_dataset);
+    }
+    
+    SimpleDEMReader::SimpleDEMReader(const std::string &filename, 
+        OGRSpatialReference destinationSRS, int windowTop, int windowBottom, int windowRight, int windowLeft)
+    {
+        windowedMode = true;
+        this->windowTop = windowTop;
+        this->windowBottom = windowBottom;
+        this->windowRight = windowRight;
+        this->windowLeft = windowLeft;
+        height = 0;
+        width = 0;
+        depth = 0;
+        log.init("SimpleDEMReader", this);
+        this->filename = filename;
+    }
+
+    SimpleDEMReader::SimpleDEMReader(const std::string &filename, OGRSpatialReference destinationSRS)
+    {
+        windowedMode = false;
+        height = 0;
+        width = 0;
+        depth = 0;
+        log.init("SimpleDEMReader", this);
+        this->filename = filename;
+}
+
+    bool SimpleDEMReader::Open()
+    {
+#ifdef DEBUG
+        log << ccl::LDEBUG << "Open()" << log.endl;
+#endif
+        gdal_dataset = (GDALDataset *)GDALOpen(this->filename.c_str(), GA_ReadOnly);
+        if (!gdal_dataset)
+            return false;
+
+        if (gdal_dataset->GetProjectionRef() == NULL)
+        {
+            log << ccl::LERR << "Open(): GetProjectionRef() failed." << log.endl;
+            GDALClose((GDALDatasetH)gdal_dataset);
+            gdal_dataset = NULL;
+            return false;
+        }
+
+        width = gdal_dataset->GetRasterXSize();
+        height = gdal_dataset->GetRasterYSize();
+        depth = gdal_dataset->GetRasterCount();
+#ifdef DEBUG
+        log << ccl::LDEBUG << "Open(): width = " << width << log.endl;
+        log << ccl::LDEBUG << "Open(): height = " << height << log.endl;
+        log << ccl::LDEBUG << "Open(): depth = " << depth << log.endl;
+#endif
+
+        char *wkt = new char[8192];
+        strncpy(wkt, gdal_dataset->GetProjectionRef(), 8192);
+        wkt[8191] = 0;
+        if (!strlen(wkt))
+        {
+            std::string prj_filename = filename.substr(0, filename.size() - 3) + "prj";
+            FILE *PRJ = fopen(prj_filename.c_str(), "r");
+            if (PRJ)
+            {
+                fgets(wkt, 8192, PRJ);
+                fclose(PRJ);
+            }
+        }
+        wkt[8191] = 0;
+
+        if (!strlen(wkt))
+        {
+            OGRSpatialReference oSRS;
+            oSRS.SetWellKnownGeogCS("WGS84");
+            char *wktp = NULL;
+            oSRS.exportToWkt(&wktp);
+            strncpy(wkt, wktp, 8191);
+            CPLFree(wktp);
+            log << ccl::LERR << "Open(): missing projection in " << filename << " assuming geographic." << log.endl;
+
+        }
+        if (!strlen(wkt))
+        {
+            log << ccl::LERR << "Open(): missing projection wkt." << log.endl;
+            GDALClose((GDALDatasetH)gdal_dataset);
+            gdal_dataset = NULL;
+            delete[] wkt;
+            return false;
+        }
+
+        char *pwkt = wkt;
+        file_srs = new OGRSpatialReference;
+        OGRErr err = file_srs->importFromWkt((char **)&wkt);
+        delete[] pwkt;
+        if (err != OGRERR_NONE)
+        {
+            log << ccl::LERR << "Open(): unable to parse projection information." << log.endl;
+            GDALClose((GDALDatasetH)gdal_dataset);
+            gdal_dataset = NULL;
+            return false;
+        }
+
+        double geotransform[6];
+        if (gdal_dataset->GetGeoTransform(geotransform) != CE_None)
+        {
+            GDALClose((GDALDatasetH)gdal_dataset);
+            gdal_dataset = NULL;
+            return false;
+        }
+
+        this->BuildCoordinateTransformations();
+
+        spacing_x = geotransform[1];
+        bound_x_low = geotransform[0];
+        rotation_x = geotransform[2];
+
+        spacing_y = geotransform[5];
+        bound_y_low = geotransform[3];
+        rotation_y = geotransform[4];
+
+        // a post is in the middle of the space, regardless of area_or_point
+        bound_x_low = bound_x_low + (spacing_x / 2);
+        bound_y_low = bound_y_low + (spacing_y / 2);
+        bound_x_high = bound_x_low + (geotransform[1] * (width - 1));
+        bound_y_high = bound_y_low + (geotransform[5] * (height - 1));
+
+        // make geographic versions of the bounds
+        geo_bound_x_low = bound_x_low;
+        geo_bound_y_low = bound_y_low;
+        geo_bound_x_high = bound_x_high;
+        geo_bound_y_high = bound_y_high;
+        if (app_ct)
+        {
+            app_ct->Transform(1, &geo_bound_x_low, &geo_bound_y_low);
+            app_ct->Transform(1, &geo_bound_x_high, &geo_bound_y_high);
+        }
+
+#ifdef DEBUG
+        log << ccl::LDEBUG << "Open(): geotransform = [" << geotransform[0] << ", "
+            << geotransform[1] << ", " << geotransform[2] << ", "
+            << geotransform[3] << ", " << geotransform[4] << ", "
+            << geotransform[5] << ", " << "]" << log.endl;
+        log << ccl::LDEBUG << "Open(): spacing_x = " << spacing_x << log.endl;
+        log << ccl::LDEBUG << "Open(): spacing_y = " << spacing_y << log.endl;
+        log << ccl::LDEBUG << "Open(): bound_x_low = " << bound_x_low << log.endl;
+        log << ccl::LDEBUG << "Open(): bound_x_high = " << bound_x_high << log.endl;
+        log << ccl::LDEBUG << "Open(): bound_y_low = " << bound_y_low << log.endl;
+        log << ccl::LDEBUG << "Open(): bound_y_high = " << bound_y_high << log.endl;
+#endif
+
+        return true;
+    }
+
+    int SimpleDEMReader::getWidth() const
+    {
+        return width;
+    }
+
+    int SimpleDEMReader::getHeight() const
+    {
+        return height;
+    }
+    void SimpleDEMReader::getMBR(double &north, double &south, double &east, double &west)
+    {
+        north = geo_bound_y_high;
+        south = geo_bound_y_low;
+        east = geo_bound_x_high;
+        west = geo_bound_x_low;
+        return;
+    }
+    // Convert each post to a double value and return it as an array
+    bool SimpleDEMReader::getGrid(std::vector<double> &grid)
+    {
+        int len = width * height;
+        double *fgrid = new double[len];
+        GDALRasterBand *poBand = gdal_dataset->GetRasterBand(1);
+        poBand->RasterIO(GF_Read, 0, 0, width, height,
+            fgrid, width, height, GDT_Float64,
+            0, 0);
+        grid.resize(len);
+        for (int i = 0; i < len; i++)
+        {
+            grid[i] = fgrid[i];
+        }
+        delete fgrid;
+        return true;
+    }
+
+    //!< Needed because of https://github.com/OSGeo/proj.4/issues/226 (deadlock/crash because of setlocal not being threadsafe)
+    static ccl::mutex transformMutex;
+    void SimpleDEMReader::BuildCoordinateTransformations()
+    {
+#ifdef DEBUG
+        log << ccl::LDEBUG << "BuildCoordinateTransformations()" << log.endl;
+#endif
+        file_ct = NULL;
+        app_ct = NULL;
+
+        // Using the C handle functions to avoid DLL hell with release/debug versions
+        if (app_srs)
+            OSRDestroySpatialReference(OGRSpatialReferenceH(app_srs));
+        app_srs = (OGRSpatialReference *)OSRNewSpatialReference(NULL);;
+        OGRErr err = app_srs->SetFromUserInput("WGS84");
+        if (err)
+            throw std::runtime_error("invalid reference type");
+        if (!file_srs)
+        {
+            file_srs = app_srs; //throw std::runtime_error("missing file spacial reference");
+            return;
+        }
+
+        // we don't need transforms if the file is geographic
+        if (file_srs->IsGeographic())
+            return;
+
+        transformMutex.lock();
+        file_ct = OGRCreateCoordinateTransformation(app_srs, file_srs);
+        app_ct = OGRCreateCoordinateTransformation(file_srs, app_srs);
+        transformMutex.unlock();
+    }
+
+}
