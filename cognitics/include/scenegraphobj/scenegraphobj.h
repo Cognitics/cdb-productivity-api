b36f23d3fb8d44d4045b58abac51a4a61ac2089a
kevinbentley <kevin.bentley@gmail.com>
2019-10-04 07:45:26 -0600

    OBJ reader.

--- cognitics/include/scenegraphobj/scenegraphobj.h
+++ cognitics/include/scenegraphobj/scenegraphobj.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include "scenegraph/Scene.h"
 #include <ctl/DelaunayTriangulation.h>
 
 namespace scenegraph
 {
 	bool buildObjFromScene(const std::string outputName, ctl::DelaunayTriangulation *dt, double lWidth, double lNorth, double width, double height);
+    Scene *buildSceneFromOBJ(const std::string &filename, bool setTexturePath);
 }
