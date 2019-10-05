17dffc2736533469937be4ad3212a2db2e402305
kevinbentley <kevin.bentley@gmail.com>
2019-10-04 20:20:44 -0600

    Fixes for libpng. Make sure and get the latest ThirdParty.7z and extract it.

--- cognitics/ObjTest/ObjTest.cpp
+++ cognitics/ObjTest/ObjTest.cpp
@@ -2,7 +2,7 @@
 #include <ccl/LogStream.h>
 #include <ccl/Timer.h>
 #include "scenegraphobj/scenegraphobj.h"
-
+#include "ip/pngwrapper.h"
 ccl::ObjLog logger;
 
 int main(int argc, char **argv)
@@ -12,6 +12,11 @@ int main(int argc, char **argv)
 
     ccl::Log::instance()->attach(ccl::LogObserverSP(new ccl::LogStream(ccl::LDEBUG)));
     scenegraph::Scene *scene = scenegraph::buildSceneFromOBJ("E:/TestData/Tile_+001_+024/Tile_+001_+024_L20.obj", "E:/TestData/Tile_+001_+024/");
+    
+    ip::ImageInfo info;
+    ccl::binary buf;
+    ip::GetPNGImagePixels("e:/test.png", info, buf);
 
+    
     return 0;
-}
+} 
