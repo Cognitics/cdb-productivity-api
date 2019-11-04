e5e2cce25d0b76998a0091eee87c29f03cd6c921
Kevin Bentley <kbentley@cognitics.net>
2019-11-04 00:25:26 +0000

    Minor fixes for linux

--- cognitics/mesh2cdb/meshrender.cpp
+++ cognitics/mesh2cdb/meshrender.cpp
@@ -346,6 +346,7 @@ void renderToFile(RenderJob &job)
     int width = 1024;
     int height = 1024;
     int depth = 3;
+    GLenum glErr;
 
     if (scene)
         delete scene;
@@ -378,7 +379,10 @@ void renderToFile(RenderJob &job)
     // The framebuffer, which regroups 0, 1, or more textures, and 0 or 1 depth buffer.
     GLuint FramebufferName = 0;
     glGenFramebuffers(1, &FramebufferName);
+    glErr = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+
     glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
+
     // The texture we're going to render to
     GLuint renderedTexture;
     glGenTextures(1, &renderedTexture);
@@ -386,12 +390,15 @@ void renderToFile(RenderJob &job)
     // "Bind" the newly created texture : all future texture functions will modify this texture
     glBindTexture(GL_TEXTURE_2D, renderedTexture);
 
-    // Give an empty image to OpenGL ( the last "0" )
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
-
     // Poor filtering. Needed !
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    // Give an empty image to OpenGL ( the last "0" )
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+
+    
 
     // Set "renderedTexture" as our colour attachement #0
     glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, renderedTexture, 0);
@@ -401,9 +408,10 @@ void renderToFile(RenderJob &job)
     glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
 
     // Always check that our framebuffer is ok
-    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    auto err = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+    if (err != GL_FRAMEBUFFER_COMPLETE)
     {
-        logger << "Frame Buffer Error!" << logger.endl;
+        logger << "Frame Buffer Error! (" << err << ")" << logger.endl;
         return;
     }
 
